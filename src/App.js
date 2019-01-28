475df010675d36bd35e7a62d6aa3c1dd6ca408b3
psy901 <psy901@gmail.com>
2019-01-28 15:22:24 -0500

    Update readme and add minor changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      email: ''
+    };
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const rootRef = firebase
+      .database()
+      .ref()
+      .child('speed');
+
+    let newUser = rootRef.push();
+    newUser.set({
+      username: this.state.username,
+      email: this.state.email
+    });
+    
+  };
+
+  handleChange = e => {
+    let change = {};
+    change[e.target.name] = e.target.value;
+    this.setState(change);
+  };
+
   render() {
+    // console.log('render')
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter Username: </label>
+          <input
+            type="text"
+            name="username"
+            // value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <br />
+          <label>Enter Email: </label>
+          <input
+            type="text"
+            name="email"
+            // value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD0q4t4YzY97xm18lpE6usL3KPDOq04Bf4",
+  authDomain: "mas-hw.firebaseapp.com",
+  databaseURL: "https://mas-hw.firebaseio.com",
+  projectId: "mas-hw",
+  storageBucket: "mas-hw.appspot.com",
+  messagingSenderId: "1031804683655"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
