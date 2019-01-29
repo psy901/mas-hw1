e061044720f330b920e0affdd15b0f901fc0199d
psy901 <psy901@gmail.com>
2019-01-29 10:22:53 -0500

    include config file for now

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyD0q4t4YzY97xm18lpE6usL3KPDOq04Bf4",
+  authDomain: "mas-hw.firebaseapp.com",
+  databaseURL: "https://mas-hw.firebaseio.com",
+  projectId: "mas-hw",
+  storageBucket: "mas-hw.appspot.com",
+  messagingSenderId: "1031804683655"
+};
+
+export { config }
